acbee133f906c7ea813ab63f101ae6f88aa531d0
WallyNegima <u.str.gm@gmail.com>
2017-05-22 13:40:31 +0900

    sample.cを追加

--- Practice/sample.c
+++ Practice/sample.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
-#include <string.g>
+#include <stdlib.h>
+#include <string.h>
 #include <wiringPi.h>
 #include <wiringSerial.h>
 
-#include "STBAP.h"
+#include "STBAPI.h"
 #include "STBCommonDef.h"
 #include "STBTypedef.h"
 
 int main(){
   //シリアルポートをオープン
-  int fd = serialOpen("");
+  int fd = serialOpen("/dev/ttyACM0", 9600);
   if(fd<0){
     printf("can not ipen serialport");
   }
 
   while(1){
-    //受信処理
-    while(serialDataAvail(df){
+    //送信処理
+		int input = 0x00;
+		char command[1];
+		itoa(input, command, 16);
+    serialPuts(fd,command);
+		
+
+		//受信処理
+    while(serialDataAvail(fd)){
       printf("recieve %c", serialGetchar(fd));
       fflush(stdout);
     }
-
-    //送信処理
-    serialPuts(fd, "hello world¥n");
 
     delay(100);
   }
