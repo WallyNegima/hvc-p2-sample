ad78c2c05281fc826cc803277f7bcbd51f75679f
WallyNegima <u.str.gm@gmail.com>
2017-05-23 13:24:04 +0900

    pythpnファイルにてコマンドを送る、返答を見ることができるようになりました！

--- Practice/test.py
+++ Practice/test.py
@@ -1,11 +1,13 @@
 import serial
 import time
+#import usb.core
+#import usb.utill
 
-ser = serial.Serial('/dev/tty.usbmodem1', 9600)
+ser = serial.Serial('/dev/ttyACM0', 9600)
 time.sleep(2)
-ser.write(0xFE000)
+ser.write(bytes([0xFE, 0x00, 0x00, 0x00]))
 print(u'send')
-line = ser.read(10)
+line = ser.read(20)
 
 print(line)
 ser.close()
