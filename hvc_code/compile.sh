45132e30543bde18aec3b324c5a55e20cdedbd3d
WallyNegima <u.str.gm@gmail.com>
2017-05-29 18:14:48 +0900

    帰ってきたー！！

--- hvc_code/compile.sh
+++ hvc_code/compile.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 gcc -o hvc main.c -I/usr/local/include -L/usr/local/lib -lwiringPi
+./hvc
--- hvc_code/main.c
+++ hvc_code/main.c
@@ -5,9 +5,8 @@
 #include <wiringSerial.h>
 #include <sys/ioctl.h>
 
-#define serialPath "/dev/ttyACM0"
-#define baudrate (9600)
-
+const char* serialPath = "/dev/ttyACM0";
+const int baudrate = 9600;
 
 int main(){
   /*シリアルオープン*/
@@ -15,30 +14,41 @@ int main(){
   if(fd<0){
     printf("can not open serialPort");
   }else{
-    printf("connected!");
+    printf("connected! fd:%d\n", fd);
   }
-	uint8_t command[5];
-	//command = (unsigned char*)malloc(sizeof(unsigned char)*4);
+	unsigned char *command;
+	command = (unsigned char*)malloc(sizeof(unsigned char)*5);
 	command[0] = 0xFE;
 	command[1] = 0x00;
 	command[2] = 0x00;
 	command[3] = 0x00;
-	serialPuts(fd, (char*)command);
-	while(1){
-		if(serialDataAvail(fd) != 0){
-			while(serialDataAvail(fd)){
-			//printf("%x\n", serialGetchar(fd));
-			uint8_t x;
-			read(fd, &x, 1);
-			printf("%x\n",x);
-			printf(" - geted data\n");
-			fflush(stdout);
-			}	
-			break;
-		}
+	command[4] = '\n';
+	//unsigned char* command = { 0xFE, 0x00, 0x00, 0x00 };
+
+	int i;
+	for(i=0; i<5; i++){
+		serialPutchar(fd, command[i]);
 	}
-	
+	delay(1000);
+	//serialPuts(fd,command);
+	//delay(1000);
+	for(i=0; i<4; i++){
+		printf("%x ", command[i]	);
+	}
+	//int aaa;
+	//scanf("%d", &aaa);
+	printf("some data arrived!\n");
+	if(!serialDataAvail(fd)){
+		printf("kitenai\n");
+	}
+	while(serialDataAvail(fd)){
+		printf("%x\n", serialGetchar(fd));
+		uint8_t x;
+		printf(" - geted data\n");
+		fflush(stdout);
+	}
 	serialClose(fd);
+
 	printf("serial closed\n");
 	return 0;
 }
